aea9391bf15e45ea494e67b77311fe130ce68d54
b17java0102 <bansalmandeep97810@gmail.com>
2017-03-26 10:52:48 +0530

    Add files via upload

---
+++ loginservlet1.java
@@ -0,0 +1,114 @@
+
+package pak1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class loginservlet1 extends HttpServlet {
+
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+       
+           PrintWriter out = response.getWriter();
+            try
+            {
+           Class.forName("com.mysql.jdbc.Driver");//Loading the driver
+
+Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/database1", "root" , "");//Establishing the connection
+           
+
+
+System.out.println("connetion Established");
+
+
+           
+            Statement stm1=con.createStatement();//creating the statement
+
+    ResultSet rs=stm1.executeQuery("select * from registerlogin");//executing the query
+  String s=request.getParameter("user") ;
+  String p=request.getParameter("pwd");
+while(rs.next())//processing the result
+{
+if(s.equals(rs.getString(1)))
+{
+    out.println("valid user name");
+}
+else
+{
+    out.println("invlid user");
+}
+if(p.equals(rs.getString(2)))
+{ 
+     out.println("valid user password");
+}
+else
+{
+    out.println("invlid password");
+}
+}
+}
+
+
+        
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        
+    }
+ 
+
+
+        
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
