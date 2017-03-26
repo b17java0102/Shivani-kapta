9eed2461e342dab3883db7e3aedd6c78c7ba1b67
b17java0102 <bansalmandeep97810@gmail.com>
2017-03-26 11:26:33 +0530

    Update interservlet -communication through RequestDispatcher.java

--- interservlet -communication through RequestDispatcher.java
+++ interservlet -communication through RequestDispatcher.java
@@ -1,4 +1,4 @@
-
+//inter-servlet  communication through RequestDispatcher
 package pac1;
 
 import java.io.IOException;
@@ -40,43 +40,82 @@ public void init()
              out.println(e);
          }
     }
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+   
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
+}
+
+
+    
+  //login servlet communicate with admin servlet--->adminservlet  
+    
+ 
+package pac1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class adminservlet extends HttpServlet {
+
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+       PrintWriter out = response.getWriter();
+         try
+               {
+            out.println("hello"+request.getParameter("user"));
+            
+        }
+         catch(Exception e)
+         {
+             out.println(e);
+         }
+    }
+
+   
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+ 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
 
 }
+
+
+
